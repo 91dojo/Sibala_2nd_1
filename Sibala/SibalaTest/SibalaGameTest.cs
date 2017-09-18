f002207b444d5f7d7885b7c080c54db1c998ea64
Andy Tsai Peng Yen <andy.tsai@titansoft.com.sg>
2017-09-18 22:33:22 +0800

    Add SibalaGame tests

--- Sibala/SibalaTest/SibalaGameTest.cs
+++ Sibala/SibalaTest/SibalaGameTest.cs
@@ -2,10 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sibala;
 
-namespace SibalaTest
+namespace Sibala.Tests
 {
-    class SibalaGameTest
+    [TestClass()]
+    public class SibalaGameTest
     {
+        [TestMethod]
+        public void SibalaGameTest_DifferentNumbers_NoPoint()
+        {
+            var target = new SibalaGame(new List<int> { 1, 2, 3, 4 });
+            var result = target.GetResultString();
+
+            Assert.AreEqual("沒點", result);
+        }
+
+        [TestMethod]
+        public void SibalaGameTest_SameNumbers_OneColor()
+        {
+            var target = new SibalaGame(new List<int> { 3, 3 ,3,3 });
+            var result = target.GetResultString();
+
+            Assert.AreEqual("一色", result);
+        }
+
+        [TestMethod]
+        public void SibalaGameTest_TwoSameNumbersAndTwoDifferentNumbers_NormalPoint()
+        {
+            var target = new SibalaGame(new List<int> { 6, 6, 1, 4 });
+            var result = target.GetResultString();
+
+            Assert.AreEqual("5點", result);
+        }
+
+        [TestMethod]
+        public void SibalaGameTest_TwoSameNumbersAndTwoDifferentNumbersSumThree_BG()
+        {
+            var target = new SibalaGame(new List<int> { 6, 6, 1, 2 });
+            var result = target.GetResultString();
+
+            Assert.AreEqual("BG", result);
+        }
+
+        [TestMethod]
+        public void SibalaGameTest_TwoSameNumbersAndTwoDifferentNumbersSumTwelve_十八啦()
+        {
+            var target = new SibalaGame(new List<int> { 5, 5, 6, 6 });
+            var result = target.GetResultString();
+
+            Assert.AreEqual("十八啦", result);
+        }
+
     }
 }
