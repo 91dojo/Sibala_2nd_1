9f8229d040b1cc88c8de7c5dfb7452b53aab4d5c
Andy Tsai Peng Yen <andy.tsai@titansoft.com.sg>
2017-09-18 18:02:38 +0800

    Compare function and unfinished test

--- Sibala/Sibala/Sibala.cs
+++ Sibala/Sibala/Sibala.cs
@@ -5,12 +5,25 @@ using System.Text;
 
 namespace Sibala
 {
-    public class Sibala
+    public  class Sibala
     {
-        public int Compare(SibalaGame source, SibalaGame target)
+        public static int Compare(SibalaGame source, SibalaGame target)
         {
-            
+            var sourceObject = source.GetResult();
+            var targetObject = target.GetResult();
 
+            if (sourceObject.type > targetObject.type)
+            {
+                return 1;
+            }
+            else if (sourceObject.type < targetObject.type)
+            {
+                return -1;
+            }
+            else
+            {
+                return sourceObject.value - targetObject.value;
+            }
         }
     }
 }
---
+++ Sibala/SibalaTest/SibalaTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sibala;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Sibala.Tests
+{
+    [TestClass()]
+    public class SibalaTests
+    {
+        [TestMethod()]
+        public void CompareTest()
+        {
+            var source = new SibalaGame(new List<int>() {1,1,1,1});
+            var target = new SibalaGame(new List<int>() {1,1,1,2});
+
+
+            Assert.Fail();
+        }
+    }
+}
+
