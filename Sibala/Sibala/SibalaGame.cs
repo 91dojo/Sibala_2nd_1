8e7a1b5e304f84152e0b76da839054c4c1a66694
ClaudeLin <linc27595@gmail.com>
2017-09-18 17:37:38 +0800

    create sibalagame class

---
+++ Sibala/Sibala/SibalaGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sibala
+{
+    public class SibalaGame
+    {
+        private List<int> _dicePoints;
+
+        public SibalaGame(List<int> dicePoints)
+        {
+            this._dicePoints = dicePoints;
+
+        }
+
+
+    }
+}
