bcee0053431e742f538b4b9667e1d17e3747c972
Andy Tsai Peng Yen <andy.tsai@titansoft.com.sg>
2017-09-18 18:26:28 +0800

    Add new feature for Compare

--- Sibala/Sibala/Sibala.cs
+++ Sibala/Sibala/Sibala.cs
@@ -7,6 +7,16 @@ namespace Sibala
 {
     public  class Sibala
     {
+        private static Dictionary<int, int> _OneColorOrder = new Dictionary<int, int>
+        {
+            {1, 6},
+            {4, 5},
+            {6, 4},
+            {5, 3},
+            {3, 2},
+            {2, 1}
+        };
+
         public static int Compare(SibalaGame source, SibalaGame target)
         {
             var sourceObject = source.GetResult();
@@ -22,7 +32,21 @@ namespace Sibala
             }
             else
             {
-                //if (sourceObject.type == )
+                if (sourceObject.type == SibalaResultType.OneColor)
+                {
+                    // 1 4 6 5 3 2 
+                    var sourceOrder = _OneColorOrder[sourceObject.value];
+                    var targetOrder = _OneColorOrder[targetObject.value];
+
+                    return sourceOrder - targetOrder;
+                }
+                else if (sourceObject.type == SibalaResultType.NormalPoint)
+                {
+                    if (sourceObject.value == targetObject.value)
+                    {
+                        return sourceObject.maxValue - targetObject.maxValue;
+                    }
+                }
                 return sourceObject.value - targetObject.value;
             }
         }
